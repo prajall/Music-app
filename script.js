7d850f41b0b5a4fdd56a62a60f3d0515401c9ae7
prajall <prajalmhrzn@gmail.com>
2022-12-04 12:26:46 +0545

    commit

--- script.js
+++ script.js
@@ -15,9 +15,28 @@ function showSongs(song) {
   song.forEach((data) => {
     let songDiv = document.createElement("div");
     songDiv.className =
-      "music sm:w-3/4 md:w-2/3 lg:w-1/2 mx-auto m-4 p-4 rounded-2xl bg-stone-50/20 flex justify-between align-center";
+      "music music sm:w-3/4 md:w-2/3 lg:w-1/2 mx-auto m-4 p-4 rounded-2xl bg-stone-50/20 md:flex justify-between align-center";
     songDiv.innerHTML = `
-    <div class="detail flex items-center">
+            <div class="detail flex w-full justify-center md:justify-start md:w-1/2 items-center">
+                <img src="${data.album.cover}" class="w-16 h-16 rounded-md">
+                <div class="">
+                    <p class="song-title mx-4 text-cyan-50 text-xl tracking-wider ">${data.title}</p>
+                    <p class="artist mx-4 text-cyan-200 text-xs">${data.artist.name}</p>
+                </div>
+            </div>
+            <div class="play items-center md:w-1/2 mt-3 md:mt-0 mx-auto flex">
+                <audio controls class="bg-none mx-auto">
+                    <source src="${data.preview}">
+                </audio>
+            </div>`;
+
+    songContainer.appendChild(songDiv);
+    //   console.log(songContainer);
+  });
+}
+
+{
+  /* <div class="detail flex items-center">
       <img src="${data.album.cover}" class="w-16 h-16 rounded-md">
       <div>
         <p class="song-title mx-4 text-cyan-50 text-xl tracking-wider">${data.title}</p>
@@ -28,9 +47,5 @@ function showSongs(song) {
       <audio controls >
         <source src = '${data.preview}'>
       </audio>
-    </div>`;
-
-    songContainer.appendChild(songDiv);
-    //   console.log(songContainer);
-  });
+    </div> */
 }
